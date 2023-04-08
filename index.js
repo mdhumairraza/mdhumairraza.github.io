d1650c2f9bbfc317d211d2f970ce50ea3d19a55d
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-08 15:11:08 +0530

    portfolio

--- index.js
+++ index.js
@@ -148,26 +148,3 @@ else
 
 
 let calander = document.querySelector(".react-activity-calendar");
-
-
-
-
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-  
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
